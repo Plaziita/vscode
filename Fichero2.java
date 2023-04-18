1253fedc2869951a4cc7cf0e17bf24ef82c783e0
Plaziita <plazadiazmario@gmail.com>
2023-04-18 14:44:28 +0200

    COMMIT

---
+++ Fichero2.java
@@ -0,0 +1,9 @@
+public class Fichero2 {
+    
+@Override
+public String toString() {
+    // TODO Auto-generated method stub
+    return super.toString();
+}
+
+}
---
+++ fichero.java
@@ -0,0 +1,14 @@
+
+public class fichero {
+
+
+
+/**
+ * @param args
+ */
+public public static void main(String[] args) {
+    
+System.out.println("Plaza");
+
+}
+}
